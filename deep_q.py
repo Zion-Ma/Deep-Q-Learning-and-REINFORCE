846cf6741585d71d798509e56f6efc5ff8708197
Dave Lubawski <77742798+DingerDave@users.noreply.github.com>
2024-11-22 11:30:17 -0500

    update deep_q

--- deep_q.py
+++ deep_q.py
@@ -13,6 +13,18 @@ class DeepQModel(tf.keras.Model):
         self.state_size = state_size
 
         # TODO: Define network parameters and optimizer
+
+        # We require that you use tf.keras.Sequential to define the model and call it self.model
+        #   (This is for auto-grading purposes)
+        self.model = ???
+        
+        # We require that you call your target model self.target_model
+        #    (This is for auto-grading purposes)
+        #    Hints: You can clone the model using tf.keras.models.clone_model
+        #           You can get the weights of model using get_weights
+        #           You can set the weights of target_model using set_weights
+
+        self.target_model = ???
         raise NotImplementedError
 
     def call(self, states):
